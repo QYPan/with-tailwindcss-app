f783b9790f64b7cdeb82a24e8b098cfcdbab1fb4
PQY <qypan1994@163.com>
2022-10-07 23:07:38 +0800

    class 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,43 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="px-8 py-12">
-      <img className="h-10" src="vercel.svg" alt="first picture" />
-      <img
-        className="mt-6 rounded-lg shadow-xl"
-        src="barrel.jpg"
-        alt="second picture"
-      />
-      <h1 className="mt-6 text-2xl font-bold text-gray-900">
-        You can work from anywhere.{" "}
-        <span className="text-indigo-500">Take advantage of it.</span>
-      </h1>
-      <p className="mt-2 text-gray-600">
-        Workcation helps you find work-friendly rentals in beautiful locations
-        so you can enjoy some nice weather even when you're not on vacation.
-      </p>
-      <div className="mt-4">
-        <a
-          className="inline-block px-5 py-3 rounded-lg shadow-lg uppercase bg-indigo-500 text-white tracking-wider font-semibold text-sm"
-          href="#"
-        >
-          Book your escape
-        </a>
+    <div className="bg-gray-300">
+      <div className="bg-gray-100 lg:grid grid-cols-2 2xl:grid-cols-5">
+        <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
+          <div className="xl:max-w-xl">
+            <img className="h-10" src="vercel.svg" alt="first picture" />
+            <img
+              className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover sm:object-center lg:hidden"
+              src="barrel.jpg"
+              alt="second picture"
+            />
+            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+              You can work from anywhere.
+              <br className="hidden lg:inline" />
+              <span className="text-indigo-500">Take advantage of it.</span>
+            </h1>
+            <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+              Workcation helps you find work-friendly rentals in beautiful
+              locations so you can enjoy some nice weather even when you're not
+              on vacation.
+            </p>
+            <div className="mt-4 sm:mt-6">
+              <a
+                className="inline-block px-5 py-3 rounded-lg shadow-lg uppercase bg-indigo-500 text-white tracking-wider font-semibold text-sm sm:text-base"
+                href="#"
+              >
+                Book your escape
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="hidden relative lg:block 2xl:col-span-3">
+          <img
+            className="absolute inset-0 w-full h-full lg:object-cover lg:object-center"
+            src="barrel.jpg"
+            alt="second picture"
+          />
+        </div>
       </div>
     </div>
   );
