23f9462f74c9e03ea44d7d3cabbf9b168260bcef
PQY <qypan1994@163.com>
2022-10-09 18:22:21 +0800

    class 6

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import popularDestinations from "../data/popularDestinations";
+import DestinationCard from "../components/DestinationCard";
 
 const Home: NextPage = () => {
   return (
@@ -44,6 +44,17 @@ const Home: NextPage = () => {
             alt="second picture"
           />
         </div>
+      </div>
+      <div className="max-w-md sm:max-w-xl lg:max-w-6xl max-auto px-8 lg:px-12 py-8">
+        <h2 className="text-xl text-gray-900">Popular destinations</h2>
+        <p className="mt-2 text-gray-600">
+          A selection of great work-friendly cities with lots to see and
+          explore.
+        </p>
+        <div className="mt-6 grid gap-6 lg:grid-cols-2 xl:grid-cols-3"></div>
+        {popularDestinations.map((destination) => (
+          <DestinationCard destination={destination} key={destination.city} />
+        ))}
       </div>
     </div>
   );
